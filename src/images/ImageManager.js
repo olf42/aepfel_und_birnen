df05c60be87f93dd85663a2180844268c326989a
Florian Rämisch <raemisch@ub.uni-leipzig.de>
2018-05-25 18:14:09 +0200

    added new assets (aepfel und birnen)

--- src/images/ImageManager.js
+++ src/images/ImageManager.js
@@ -5,10 +5,28 @@ export default class ImageManager {
     constructor() {
         this.images = {
             "apples": {
-                "apple1": "./assets/apple.png"
+                "apple01": "./assets/apfel_01_klein.png",
+                "apple02": "./assets/apfel_02_klein.png",
+                "apple03": "./assets/apfel_03_klein.png",
+                "apple04": "./assets/apfel_04_klein.png",
+                "apple05": "./assets/apfel_05_klein.png",
+                "apple06": "./assets/apfel_06_klein.png",
+                "apple07": "./assets/apfel_07_klein.png",
+                "apple08": "./assets/apfel_08_klein.png",
+                "apple09": "./assets/apfel_09_klein.png",
+                "apple10": "./assets/apfel_10_klein.png"
             },
             "pears": {
-                "pear1": "./assets/pear.png"
+                "pear01": "./assets/birne_01_klein.png",
+                "pear02": "./assets/birne_02_klein.png",
+                "pear03": "./assets/birne_03_klein.png",
+                "pear04": "./assets/birne_04_klein.png",
+                "pear05": "./assets/birne_05_klein.png",
+                "pear06": "./assets/birne_06_klein.png",
+                "pear07": "./assets/birne_07_klein.png",
+                "pear08": "./assets/birne_08_klein.png",
+                "pear09": "./assets/birne_09_klein.png",
+                "pear10": "./assets/birne_10_klein.png"
             },
             "grounds": {
                 "ground1": "./assets/ground.png"
@@ -16,7 +34,7 @@ export default class ImageManager {
             "scenes": {
                 "appleDudeApple": "./assets/apfel_fuer_dude.png",
                 "appleDudeArm": "./assets/arm.png",
-                "appleDude": "./assets/dude_ohne_apfel_ohne_arm_Ausschnitt.png",   
+                "appleDude": "./assets/dude_ohne_apfel_ohne_arm_Ausschnitt.png",
             }
         }
     }
