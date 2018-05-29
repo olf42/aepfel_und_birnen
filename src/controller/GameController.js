1e10c0ed960ff56c0f549e9c55ef4de220428fbc
pmuehleder <peter.muehleder@yahoo.de>
2018-05-29 19:02:23 +0200

    random scene selection added

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -1,5 +1,21 @@
+import { shuffle } from 'lodash'
+
 export default class GameController {
     constructor () {
+
+        const scenes = [
+            "BouncyPearScene",
+            "SearchScene",
+            "DdrScene"
+        ]
+
         this.score = 0
+        this.scenes = shuffle(scenes)
+    }
+
+    nextScene () {
+        const scene = this.scenes[0]
+        this.scenes.shift()
+        return scene
     }
 }
