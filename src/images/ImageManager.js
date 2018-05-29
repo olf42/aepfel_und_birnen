29f084727a46553046f514b74807349c183ec078
Peter Mühleder <peter.muehleder@uni-leipzig.de>
2018-05-29 10:58:39 +0200

    winking lady added to ddr scene

--- src/images/ImageManager.js
+++ src/images/ImageManager.js
@@ -35,6 +35,8 @@ export default class ImageManager {
                 "appleDudeApple": "./assets/apfel_fuer_dude.png",
                 "appleDudeArm": "./assets/arm.png",
                 "appleDude": "./assets/dude_ohne_apfel_ohne_arm_Ausschnitt.png",
+                "winkingLady": "./assets/lady_zwinker.png",
+                "winkingLadyLid": "./assets/lady_zwinker_lid.png"
             }
         }
     }
